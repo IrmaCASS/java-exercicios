066152909f66d91bf4d507d79f1571c08b51b333
Irma Assunção <irmacristinaphb@gmail.com>
2026-04-13 22:21:13 -0300

    exercicio 22 - quociente e resto da divisao

---
+++ src/Ex22QuocienteERestoDaDivisao.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Ex22QuocienteERestoDaDivisao {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(">>>>CALCULADORA DE DIVISÃO<<<<");
+        System.out.println("Digite o primeiro numero: ");
+        double numero1 = sc.nextDouble();
+        System.out.println("Digite o segundo número: ");
+        double numero2 = sc.nextDouble();
+        double quociente = numero1 / numero2;
+        double resto = numero1 % numero2;
+        System.out.printf("O quociente da divisão é: %.2f . E o resto da divisão é: %.2f%n", quociente, resto);
+    }
+}
